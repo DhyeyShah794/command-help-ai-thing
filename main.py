4e0086b8477a741c55a24fcb6ac50c8f27d488e2
dhyey <shah.dhyey@gmail.com>
2023-02-05 13:15:38 +0530

    Made basic CLI & encrypted API key using fernet

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
+import click
+from cryptography.fernet import Fernet
 import openai
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-openai.api_key = os.environ.get('APIKey')
+encrypted = os.environ.get('APIKey')
+encryption_key = os.environ.get('EncryptionKey')
+f = Fernet(encryption_key)
+decrypted = f.decrypt(encrypted.encode())
+openai.api_key = decrypted.decode()
 
-model_engine = "text-davinci-003"
-prompt = input("Enter your prompt: ")
+@click.group()
+def commands():
+    pass
 
-completions = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=100,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+@click.command()
+@click.argument("prompt", default="default")
+@click.option("--engine", default="text-ada-001", required=1, help="The engine to use for the AI. (Default: text-davinci-003)")
+@click.option("--randomness", default=0.5, required=1, help="The randomness/creativity of the answer. (Default: 0.5)")
+def ask(prompt, engine, randomness):
+    """Prompt for the AI to respond to."""
+    if prompt == "default":
+        click.echo("Command Help AI Thing is running. Use the --prompt option to prompt the AI to respond to a question.")
+    else:
+        completions = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=randomness,
+        )
+        response = completions.choices[0].text
+        click.echo(response)
 
-message = completions.choices[0].text
-print(message)
+commands.add_command(ask)
+
+if __name__ == '__main__':
+    commands()
